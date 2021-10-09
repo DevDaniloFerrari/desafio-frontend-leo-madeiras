f38c6f044fdcdb9bdac545dfcad7b799b093eed0
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-10-09 08:23:28 -0300

    colocando mascara

--- src/pages/PersonForm/index.jsx
+++ src/pages/PersonForm/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { addPerson } from '../../shared/services/people.service'
 import { useForm } from "react-hook-form";
+import InputMask from 'react-input-mask';
 
 export default function PersonForm() {
     const { register, handleSubmit, reset } = useForm();
@@ -13,6 +14,13 @@ export default function PersonForm() {
     return (
         <div>
             <input type="text" {...register("nome", { required: true })} />
+            <InputMask mask="999.999.999-99" {...register("cpf", { required: true })}>
+                {(inputProps) => <input {...inputProps} type="text" disableUnderline />}
+            </InputMask>
+            <InputMask mask="(99) 9 9999-9999" {...register("telefone", { required: true })}>
+                {(inputProps) => <input {...inputProps} type="tel" disableUnderline />}
+            </InputMask>
+            <input type="email" {...register("email", { required: true })} />
             <button onClick={handleSubmit(onSubmit)}>adicionar</button>
         </div>
     )
